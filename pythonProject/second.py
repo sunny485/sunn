b496484c3199efdbe30073fc0b9c0b85ee852826
sunny485 <58816493+sunny485@users.noreply.github.com>
2020-09-08 18:51:43 +0600

    Add files via upload

---
+++ pythonProject/second.py
@@ -0,0 +1,27 @@
+#in python if you want to write comment you have to use "#" this is a single line comment
+#and you want to comment out multi line comment you have to use """ but with opning door and closing door
+#just like taht
+"""i am learning pyhon
+and also c and web deplepment caurse"""
+print("hello sunny\n learning hard\t j")
+import os
+import flask
+
+# print statement for printing strings
+print("Harry is a programmer")
+
+# Print statement with a literal
+print(1+87)
+
+#This will print "Harry is a programmer" and 88 on the screen respectively!
+import os
+import flask
+
+# print statement for printing strings
+print("Harry is a programmer", end="")
+
+# Print statement with a literal
+print("sunny")
+
+#This will print "Harry is a programmer**88" on the screen
+#if you waNT TO YOU EVERY print program is running in one line you have to use end="" its give space in lines also
---
+++ pythonProject/thrd.py
@@ -0,0 +1,12 @@
+var1= "22"
+var2= "56"
+var3= 36.7
+#print(type(var1)) this for finding the type of variable used by type funtion
+#print(var2+var3)this is for calculate the veriable
+#print(var1+var2) this is not the right way to add a string variable and integer veriable
+#the right way is
+#print(int(var1)+int(var2))
+#print(10* var1)thi sthe way to type a word many times note only string words if you want to type a number like that you have to convert that number number to string
+#like that
+print(10*str(int(var1)+int(var2)))
+#this is collrd type casting
